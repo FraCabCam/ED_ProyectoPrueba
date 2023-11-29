797993980e0bc24b6dc370c01d8ce166fce9250d
FraCabCam <fcabrera4084@ceuandalucia.es>
2023-11-29 20:41:46 +0100

    PRACTICA 3

--- Proyecto3_3/src/Practica3/Clase3_3A.java
+++ Proyecto3_3/src/Practica3/Clase3_3A.java
@@ -3,7 +3,7 @@ package Practica3;
 public class Clase3_3A {
 	
 	public static void main(String[] args) {
-		System.out.println("Canela en rama,Francisco Javier Cabrera Camino");
+		System.out.println("Hola profesor soy Francisco Javier Cabrera Camino");
 	}
 
 }
